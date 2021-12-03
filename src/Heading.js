b8184d4adc8935ac36a391812fc5de054470534a
AjithRevildo <91174414+AjithRevildo@users.noreply.github.com>
2021-12-03 18:43:25 +0530

    Update Heading.js

--- src/Heading.js
+++ src/Heading.js
@@ -1,9 +1,9 @@
 export function Heading() {
     return (
       <div id="head-div">
-        <p id="head-title">Shop in Style <br></br> <span id="head-span">With this shop homepage template</span></p>
-        <p></p>
-      </div>
+        <p id="head-title">Shop in Style <br></br>
+        <span id="head-span">With this shop homepage template</span></p>
+        </div>
     );
   }
   
